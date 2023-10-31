1d7d1296db078f6079b91d28c6fb9a054070aff1
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-10-31 10:11:18 +0900

    Cardコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import Card from "./utils/Card/Card";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon/";
@@ -38,7 +39,11 @@ function App() {
         <h1>ロード中...</h1>
       ) : (
         <>
-          <h1>ポケモンデータ取得しました！</h1>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
         </>
       )}
     </div>
